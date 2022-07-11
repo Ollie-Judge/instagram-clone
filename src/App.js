a2b1973b5e671aa27d669320b11e9e85d865e3a1
Ollie-Judge <ollie.judge@hotmail.com>
2022-07-11 15:18:14 +0100

    added posts from the api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [pictures, setPictures] = useState([]);
+
+  useEffect(() => {
+    handleFetch();
+  }, []);
+
+  const handleFetch = async () => {
+    const response = await fetch("https://picsum.photos/v2/list"); // tthe response object converts the api from json
+    const data = await response.json();
+    setPictures(data);
+  };
   return (
     <div className="App">
-      <header className="App-header"></header>
+      {pictures.map((item, index) => {
+        return (
+          <div>
+            <h2>{item.author}</h2>
+            <img
+              className="postContainer"
+              src={item.download_url}
+              alt="randomImage"
+            />
+            <p>Post number {index}</p>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
