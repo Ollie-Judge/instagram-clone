3207b5f8d9cf717934b6cd6284c4f8161b845b86
Ollie-Judge <ollie.judge@hotmail.com>
2022-07-12 14:13:25 +0100

    added login/signup functionality

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
+import { useState, useEffect } from "react";
+import Image from "./components/image";
+import SignOrLog from "./components/signOrLog";
 import "./App.css";
-import { useEffect, useState } from "react";
 
 const App = () => {
-  const [pictures, setPictures] = useState([]);
+  const [user, setUser] = useState();
+  const [photos, setPhotos] = useState([]);
+
+  const fetchImages = async () => {
+    const response = await fetch("https://picsum.photos/v2/list");
+    const data = await response.json();
+    setPhotos(data);
+  };
 
   useEffect(() => {
-    handleFetch();
-  }, []);
+    fetchImages();
+  }, []); //every time user's value changes, useEffect will run again
 
-  const handleFetch = async () => {
-    const response = await fetch("https://picsum.photos/v2/list"); // tthe response object converts the api from json
-    const data = await response.json();
-    setPictures(data);
-  };
   return (
     <div className="App">
-      {pictures.map((item, index) => {
-        return (
-          <div>
-            <h2>{item.author}</h2>
-            <img
-              className="postContainer"
-              src={item.download_url}
-              alt="randomImage"
-            />
-            <p>Post number {index}</p>
-          </div>
-        );
-      })}
+      <SignOrLog setter={setUser} />
+      <h1>{user}</h1>
+      {user &&
+        photos.map((item, i) => {
+          return <Image key={i} author={item.author} url={item.download_url} />;
+        })}
     </div>
   );
 };
---
+++ src/components/signOrLog.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { signUp } from "../utils";
+
+const SignOrLog = ({ setter }) => {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    await signUp(username, email, password, setter);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <input onChange={(e) => setUsername(e.target.value)} />
+      <input onChange={(e) => setEmail(e.target.value)} />
+      <input onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default SignOrLog;
